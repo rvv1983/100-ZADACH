fd51b8517a0cfc2a400ab44037ff19adb07c5226
Vladimir Rybin <rvv1983@yandex.ru>
2021-11-08 22:27:53 +0500

    последняя  цифра трехзначного числа

---
+++ задача8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать числа от -N до N 8. Показать четные числа от 1 до N
+void FillArray(int[] numb)
+{
+    int lenght=numb.Lenght;
+    int index = 0;
+    while(index<lenght)
+    {
+        numb[index]=Array[index];
+        index++;
+    }
+}
