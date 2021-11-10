558f1d29020e761eb6fc2641e698db8a8f295b7c
Владимир Рыбин <rvv1983@yandex.ru>
2021-11-11 00:47:40 +0500

    наибольшая цифра числа отрезка [10, 99]

---
+++ задача11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+ char GetMaxNumber(int num) 
+{ 
+    string ab = Convert.ToString(num); 
+    char a = ab[0]; 
+    char b = ab[1]; 
+    if (a > b) return a; 
+    else return b; 
+} 
+int num = new Random().Next(10, 100); 
+char result = GetMaxNumber(num); 
+Console.WriteLine($"В числе {num} наибольшая цифра - {result}");
+//готов!
