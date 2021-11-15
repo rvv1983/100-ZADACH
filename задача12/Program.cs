3974da0927214188d6af3cd721e8813c10f25691
Владимир Рыбин <rvv1983@yandex.ru>
2021-11-15 21:37:11 +0500

    удалить вторую цифру трёхзначного числа - выполнен

---
+++ задача12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+class programm
+{
+    static void Main()
+    {
+        string s = "569";
+        string a = s.Remove(1,1);
+        Console.WriteLine(a);
+        Console.ReadLine();
+    }
+}
+// финиш
