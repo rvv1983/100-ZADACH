2b8a74e4828a33810abae562f85d7f029a1ce20a
Владимир Рыбин <rvv1983@yandex.ru>
2021-11-17 00:04:08 +0500

    кратно ли число заданному, если нет, вывести остаток.-finished

---
+++ задача13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+double a = 15;
+double b = 4;
+double x = 0;
+if(a%b==0)
+
+Console.WriteLine("число a кратно числу b");
+else
+Console.WriteLine (x =x+(a/b)%10);
+
+
