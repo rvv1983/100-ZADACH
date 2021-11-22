db6b585b8b529058da03286a59416d6c1976b336
Владимир Рыбин <rvv1983@yandex.ru>
2021-11-22 23:04:26 +0500

    полиндром-finished

---
+++ задача19/Program.cs
@@ -0,0 +1,12 @@
+﻿//19.  Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите 5 цифр:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(pol(a));
+string pol(int a)
+{
+    string b = Convert.ToString(a);
+    string res = $"Число {a} НЕ является палиндромом";
+    if (b[0] == b[4] && b[1] == b[3]) res = $"Число {a} является палиндромом";
+    return res;
+}
+
