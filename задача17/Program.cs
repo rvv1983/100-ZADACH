dbbe179b1da94548c69e8760ec81aba4d2c83c9b
Владимир Рыбин <rvv1983@yandex.ru>
2021-11-22 22:35:15 +0500

    проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y is  finished

---
+++ задача17/Program.cs
@@ -0,0 +1,16 @@
+﻿//17. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+/* bool func(bool x, bool y)
+{
+ bool res = false;   
+if (!(x&y)==!x|!y)
+{
+  result = true;
+}
+  return res;
+}
+Console.WriteLine(func(true,false)); */
+bool x = true;
+bool y = false;
+bool f1 = !(x || y);
+bool f2 = !x && !y;
+Console.WriteLine(f1 == f2);
