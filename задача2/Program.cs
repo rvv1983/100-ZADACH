9b90e690ecf3f274aa34b30584f52c4085fade42
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-02 23:54:05 +0500

    1

--- задача2/Program.cs
+++ задача2/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Вывести квадрат числа 
 Console.WriteLine("квадрат введенного числа равен");
 
-int a =5;
+int a=5; 
+//Console.WriteLine ("Введите первое число");
+// = Convert.ToInt32(Console.ReadLine ());
+
 
 System.Console.WriteLine(a*a);
 
