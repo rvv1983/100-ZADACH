1794f18b538de710c89ae224482655529e481874
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-05 21:58:58 +0500

    fin

---
+++ задача10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+int A = new Random().Next(100,999);
+Console.WriteLine(A);
+int ThirdDigit = A%100;
+Console.WriteLine();
+int SecondDigit = (ThirdDigit-ThirdDigit%10)/10;
+Console.WriteLine(SecondDigit+"<- second digit");
