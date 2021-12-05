1f812ba70d6e22ca914f37d6d3b373f09722c1b1
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-05 21:23:43 +0500

    fin

---
+++ задача3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два числа. Показать большее и меньшее число
+Console.WriteLine ("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine ("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (a>b)
+{
+    max=a;min=b;
+}
+else
+{
+    max=b;min=a;
+}
+Console.WriteLine(max);Console.WriteLine("^ максимальное число");
+Console.WriteLine(min);Console.WriteLine("^ минимальное число");
