bcd53fbfdeb15d0e9f9d87e50742ce595bd71585
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-05 22:37:08 +0500

    fin

---
+++ задача21/Program.cs
@@ -0,0 +1,19 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Ввведите пятизначное число:");
+string s = Console.ReadLine();
+int a, b, c, d, e;
+a = Convert.ToInt32(s);
+b = a % 10;
+c = a/10%10;
+d = a/1000%10;
+e = a/10000%10;
+if(b==e&&c==d)
+{
+   Console.WriteLine("Данное число является полиндромом");
+}
+else
+{
+   Console.WriteLine("Данное число  не является полиндромом");
+}
+ 
+
