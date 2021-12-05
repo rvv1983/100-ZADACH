84110fbecf7dce2d057814e03604a7b4d591f88c
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-05 21:30:40 +0500

    fin

---
+++ задача5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления значения функции y=f(a) 
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.Write("Вычисление sin(x)\nУгол в градусах = ");
+         double a = Convert.ToDouble(Console.ReadLine());
+         double x, y;
+         x = a * Math.PI / 180;
+         y = Math.Sin(x);
+         Console.WriteLine("sin({0})={1}",x,y);
+         Console.ReadKey();
+      }
+   }
