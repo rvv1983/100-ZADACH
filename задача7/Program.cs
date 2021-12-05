3961579ac92f3cb9b07b685d3e327cdd4ee06791
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-05 21:50:33 +0500

    fin

---
+++ задача7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа от -N до N 8. Показать четные числа от 1 до N
+void met (int N)
+{
+int count = -N;
+while (count<= N)
+{
+    Console.WriteLine(count);
+     count++;
+     
+}
+Console.WriteLine("четные от 1 до N это");
+for (int i = 1; i < N + 1; i++) if (i % 2 == 0) { Console.Write((i) + " "); }
+}
+met(7);
