8cae6c7163c196569be0c0b6d7f787bbd8e28c65
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:29:14 +0500

    fin

---
+++ задача28/Program.cs
@@ -0,0 +1,16 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+int a = new Random().Next(0, 1000);
+Console.WriteLine(a);
+
+int Sum(int s)
+{
+    int summ = 0;
+    while (s != 0)
+    {
+        summ = summ + s % 10;
+        s = s / 10;
+    }
+    return summ;
+}
+Console.WriteLine($"Сумма введенных цифр  {a} = {Sum(a)}");
