4760319e82334890cc1a1ff06f3ab6d4713fe3f6
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 21:59:52 +0500

    fin

---
+++ задача22/Program.cs
@@ -0,0 +1,7 @@
+﻿// Решение от преподавателя
+double Distance2D (double x1, double x2, double y1, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2)));
+}
+
+Console.WriteLine(Distance2D(10, 28, 2, 14));
