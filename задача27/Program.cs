6cad4f0bc61720b6a5141d7e049e7ca80f48dd9e
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:23:43 +0500

    fin

---
+++ задача27/Program.cs
@@ -0,0 +1,16 @@
+﻿//Определить количество цифр в числе
+
+int CountCheck(int n)
+{
+    int s=0;
+
+    while (n!=0)
+    {
+        s++;
+        n=n/10;
+    }
+
+    return s;
+}
+
+Console.WriteLine(CountCheck(125566));
