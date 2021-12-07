9e368f26792d35a10d4fc6de3fc01b99a0af59a7
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:46:27 +0500

    fin

---
+++ задача30/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+void ChisloVkube(int any)
+{
+    for(int i=1; i <=any; i++)
+    {
+        if(i*i*i%2==0)
+        Console.WriteLine($"Куб числа {i} = {i*i*i}");
+    }
+
+}
+Console.WriteLine("Введите число *any*, кубы чисел которого будут заканчиваться на четную цифру ");
+int any = Convert.ToInt32(Console.ReadLine());
+
+ChisloVkube(any);
