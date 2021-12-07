42a5b1d5d55fcf008e56544f1ed35a3f7bb43aea
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:36:00 +0500

    fin

---
+++ задача29/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Написать программу вычисления произведения чисел от 1 до N
+
+int MNOZH(int a)
+{
+    int s = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        s = s * i;
+    }
+    return s;
+}
+Console.Write("Введите край диапазона N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение всех чисел от 1 до {N} = {MNOZH(N)}");
