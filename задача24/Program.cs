50a3d3373f03a62cec67a37d5b9901acf9d306b6
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:09:35 +0500

    fin

---
+++ задача24/Program.cs
@@ -0,0 +1,10 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите диапазон N  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("куб каждого числа будет");
+
+for (int i = 1; i <= a ; i++)
+{
+    Console.Write(Math.Pow(i,3)+"  ");
+}
