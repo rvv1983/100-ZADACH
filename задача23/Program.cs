bb9c3c9f230283f38012e808a58ddf0f7b8e2950
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:04:02 +0500

    fin

---
+++ задача23/Program.cs
@@ -0,0 +1,11 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+int i =1;
+Console.Write("Введите N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"чмсла в квадрате диапазона от 1 до {a} равны следующим значениям");
+
+while (i<=a)
+{
+     Console.Write(Math.Pow(i,2)+"  ");
+     i++;
+}
