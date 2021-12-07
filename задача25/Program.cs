d7470af75b99e5970aef6231ea53f8c35d358c0c
Владимир Рыбин <rvv1983@yandex.ru>
2021-12-07 22:14:14 +0500

    fin

---
+++ задача25/Program.cs
@@ -0,0 +1,12 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+Console.Write("Введите крайниюю цифру диапазона A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+ int sum=0;
+for (int i = 1; i <= a; i++)
+{
+    
+     sum=sum+i; 
+}
+Console.Write($"сумма совокупности чисел равна: {sum}");
